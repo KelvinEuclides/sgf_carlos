9cc0b455ce34930a637d1176af98e0adc32bfc96
yuran09 <yuranfgs@gmail.com>
2024-06-25 15:57:38 +0000

    fix company editing

--- storage/framework/views/9eabe6b03f7536bb4d421fa19f54add0a81673a3.php
+++ storage/framework/views/9eabe6b03f7536bb4d421fa19f54add0a81673a3.php
@@ -1,15 +1,15 @@
 <?php
-<!-- use App\Subscription; -->
+use App\Subscription;
 $subscriptions = Subscription::pluck('name','id');
 ?>
-<?php echo e(Form::open(array('url'=>'users','method'=>'post'))); ?>
+<?php echo e(Form::model($user,array('route' => array('users.update', $user->id), 'method' => 'PUT'))); ?>
 
 <div class="row">
     <div class="col-md-12">
-        <div class="form-group">
-            <?php echo e(Form::label('name',__('Name'),['class'=>'form-control-label'])); ?>
+        <div class="form-group ">
+            <?php echo e(Form::label('name',('Name'),['class'=>'form-control-label'])); ?>
 
-            <?php echo e(Form::text('name',null,array('class'=>'form-control','placeholder'=>__('Enter User Name'),'required'=>'required'))); ?>
+            <?php echo e(Form::text('name',null,array('class'=>'form-control font-style','placeholder'=>('Enter User Name')))); ?>
 
             <?php $__errorArgs = ['name'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -27,9 +27,9 @@ unset($__errorArgs, $__bag); ?>
     </div>
     <div class="col-md-12">
         <div class="form-group">
-            <?php echo e(Form::label('email',__('Email'),['class'=>'form-control-label'])); ?>
+            <?php echo e(Form::label('email',('Email'),['class'=>'form-control-label'])); ?>
 
-            <?php echo e(Form::text('email',null,array('class'=>'form-control','placeholder'=>__('Enter User Email'),'required'=>'required'))); ?>
+            <?php echo e(Form::text('email',null,array('class'=>'form-control','placeholder'=>('Enter User Email')))); ?>
 
             <?php $__errorArgs = ['email'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -45,11 +45,12 @@ endif;
 unset($__errorArgs, $__bag); ?>
         </div>
     </div>
+    <?php if(\Auth::user()->type == 'super admin'): ?>
     <div class="col-md-12">
         <div class="form-group">
-            <?php echo e(Form::label('nuit',__('Nuit'),['class'=>'form-control-label'])); ?>
+            <?php echo e(Form::label('nuit',('Nuit'),['class'=>'form-control-label'])); ?>
 
-            <?php echo e(Form::text('nuit',null,array('class'=>'form-control','placeholder'=>__('Nuit'),'required'=>'required'))); ?>
+            <?php echo e(Form::text('nuit',null,array('class'=>'form-control','placeholder'=>('Nuit'),'required'=>'required'))); ?>
 
             <?php $__errorArgs = ['nuit'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -67,9 +68,9 @@ unset($__errorArgs, $__bag); ?>
     </div>
     <div class="col-md-12">
         <div class="form-group">
-            <?php echo e(Form::label('endereco',__('Endereço'),['class'=>'form-control-label'])); ?>
+            <?php echo e(Form::label('endereco',('Endereço'),['class'=>'form-control-label'])); ?>
 
-            <?php echo e(Form::text('endereco',null,array('class'=>'form-control','placeholder'=>__('Endereço'),'required'=>'required'))); ?>
+            <?php echo e(Form::text('endereco',null,array('class'=>'form-control','placeholder'=>('Endereço'),'required'=>'required'))); ?>
 
             <?php $__errorArgs = ['endereco'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -85,20 +86,18 @@ endif;
 unset($__errorArgs, $__bag); ?>
         </div>
     </div>
-   
     <div class="col-md-12">
         <div class="form-group">
-            <?php echo e(Form::label('subscription',__('Subscription'),['class'=>'form-control-label'])); ?>
+            <?php echo e(Form::label('subscription',('Subscription'),['class'=>'form-control-label'])); ?>
 
-            <?php echo Form::select('role',$subscriptions, null,array('class' => 'form-control select2','required'=>'required')); ?>
+            <?php echo Form::select('subscription',$subscriptions ?? '', null,array('class' => 'form-control select2','required'=>'required')); ?>
 
      </div>
-   
     <div class="col-md-12">
         <div class="form-group">
-            <?php echo e(Form::label('password',__('Password'),['class'=>'form-control-label'])); ?>
+            <?php echo e(Form::label('password',('Password'),['class'=>'form-control-label'])); ?>
 
-            <?php echo e(Form::password('password',array('class'=>'form-control','placeholder'=>__('Enter User Password'),'required'=>'required','minlength'=>"6"))); ?>
+            <?php echo e(Form::password('password',array('class'=>'form-control','placeholder'=>('Enter User Password'),'required'=>'required','minlength'=>"6"))); ?>
 
             <?php $__errorArgs = ['password'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -114,11 +113,12 @@ endif;
 unset($__errorArgs, $__bag); ?>
         </div>
     </div>
+    <?php endif; ?>
     <?php if(\Auth::user()->type != 'super admin'): ?>
         <div class="form-group col-md-12">
-            <?php echo e(Form::label('role', __('User Role'),['class'=>'form-control-label'])); ?>
+            <?php echo e(Form::label('role', ('User Role'),['class'=>'form-control-label'])); ?>
 
-            <?php echo Form::select('role', $roles, null,array('class' => 'form-control select2','required'=>'required')); ?>
+            <?php echo Form::select('role', $roles, $user->roles,array('class' => 'form-control select2','required'=>'required')); ?>
 
             <?php $__errorArgs = ['role'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -135,11 +135,8 @@ unset($__errorArgs, $__bag); ?>
         </div>
     <?php endif; ?>
     <div class="col-md-12 text-right">
-        <?php echo e(Form::submit(__('Create'),array('class'=>'btn btn-sm btn-primary rounded-pill'))); ?>
+        <?php echo e(Form::submit(__('Update'),array('class'=>'btn btn-sm btn-primary rounded-pill'))); ?>
 
     </div>
 </div>
-<?php echo e(Form::close()); ?>
-
-
-<?php /**PATH /var/www/resources/views/user/create.blade.php ENDPATH**/ ?>
+<?php echo e(Form::close()); ?><?php /**PATH /var/www/resources/views/user/edit.blade.php ENDPATH**/ ?>
